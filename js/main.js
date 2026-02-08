0f89c348802e97cf207d4e97b46abc7a6919861d
unknown <holymushroom1234@gmail.com>
2026-02-08 20:19:54 +0700

    adaptive is done

--- js/main.js
+++ js/main.js
@@ -12,6 +12,30 @@ $(function () {
         infinite: true,
         draggable: false,
         waitForAnimate: false,
+        responsive:
+            [
+                {
+                    breakpoint: 1100,
+                    settings: {
+                        slidesToShow: 3,
+                    },
+                },
+                {
+                    breakpoint: 750,
+                    settings: {
+                        slidesToShow: 2,
+                    },
+                },
+                {
+                    breakpoint: 550,
+                    settings: {
+                        slidesToShow: 1,
+                        draggable: true,
+                        dots: true,
+                        appendDots: $('.team__dots'),
+                    },
+                },
+            ]
     })
 
     $('.team__slider-prev').on('click', function (e) {
@@ -60,4 +84,40 @@ $(function () {
         }
     })
 
+    $(".header__nav-list a, .header__top-btn, .footer__go-top").on("click", function (e) {
+        //отменяем стандартную обработку нажатия по ссылке
+        e.preventDefault()
+        //забираем идентификатор бока с атрибута href
+        var id = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+            top = $(id).offset().top - 50
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({ scrollTop: top }, 800)
+    })
+    setInterval(() => {
+        if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+            $('.burger').addClass('burger--follow')
+        } else {
+            $('.burger').removeClass('burger--follow')
+        }
+    }, 0)
+    $('.burger, .overlay, .header__top a').on('click', function (e) {
+        e.preventDefault()
+        e.stopPropagation();
+        $('.burger').toggleClass('burger--close');
+        $('.header__top').toggleClass('header__top--open')
+        $('.overlay').toggleClass('overlay--hide')
+    })
+
+    $('.footer__top-title--slide').on('click', function () {
+        if (window.innerWidth <= 550) {
+            $(this).next().slideToggle();
+        }
+    });
+
+    $(window).on('resize', function () {
+        if (window.innerWidth > 550) {
+            $('.footer__top-title--slide').next().removeAttr('style');
+        }
+    });
 })
